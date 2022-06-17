24c8db32ae98a16f0bba2df1b0b4a1962e146f6b
Yuriy Kolosov <yuriy.kolosov@tomtom.com>
2022-06-17 12:26:07 +0300

    initial commit

---
+++ react-login-form/src/components/LoginForm.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import {PropTypes} from 'prop-types';
+
+
+const LoginForm = (onSubmit) => {
+    const [inputs, setInputs] = useState({})
+    const submitDisabled = inputs.username && inputs.password ? false : true;
+
+    const onChangeHandler = ({target: {name, value}}) => {
+        setInputs(state => ({...state, [name]:value }), [])
+    }
+
+    return (
+        <div>
+            <input value={inputs.username || ''} name='username' type='text' onChange={onChangeHandler} id='username-input' />
+            <input value={inputs.password || ''} name='password' type='password' onChange={onChangeHandler} id='password-input' />
+            <button disabled={submitDisabled} onClick={()=>onSubmit(inputs.username, inputs.password )}>Submit</button>
+        </div>
+    )
+}
+LoginForm.propTypes = {
+    onSubmit: PropTypes.object
+}
+
+export default LoginForm

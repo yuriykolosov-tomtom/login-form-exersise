a598558cd337a69dfbe08901e4c95d3b032b2664
Yuriy Kolosov <yuriy.kolosov@tomtom.com>
2022-06-17 12:29:57 +0300

    added id to button

--- react-login-form/src/components/LoginForm.jsx
+++ react-login-form/src/components/LoginForm.jsx
@@ -14,7 +14,7 @@ const LoginForm = (onSubmit) => {
         <div>
             <input value={inputs.username || ''} name='username' type='text' onChange={onChangeHandler} id='username-input' />
             <input value={inputs.password || ''} name='password' type='password' onChange={onChangeHandler} id='password-input' />
-            <button disabled={submitDisabled} onClick={()=>onSubmit(inputs.username, inputs.password )}>Submit</button>
+            <button disabled={submitDisabled} onClick={()=>onSubmit(inputs.username, inputs.password )} id='login-button'>Submit</button>
         </div>
     )
 }
